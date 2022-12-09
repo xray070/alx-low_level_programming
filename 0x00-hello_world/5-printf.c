ab6971e84b417b18c5261284c5cdbddb247c1928
xray070 <emmanuelansah@gmail.com>
2022-12-09 02:49:13 -0800

    well done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print C program with printf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
