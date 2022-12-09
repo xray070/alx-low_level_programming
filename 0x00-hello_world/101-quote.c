4b0c2ff131911b059172dbd4499f2fabe5cefac1
xray070 <emmanuelansah@gmail.com>
2022-12-09 13:10:18 -0800

    Done all

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Done
+ * Return: 1
+ *//
+int main(void)
+{
+	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",);
+	return (1);
+}
