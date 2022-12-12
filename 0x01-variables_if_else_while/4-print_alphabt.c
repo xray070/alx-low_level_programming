449616b9de25073641bf5187a91ff47750226b38
xray070 <emmanuelansah@gmail.com>
2022-12-12 05:06:55 -0800

    T4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,14 @@
 int main(void)
 {
 	int ch = 'a';
-	while (ch <= 'z');
+
+	while (ch <= 'z')
+
 	if ((ch != 'q') && (ch != 'e'))
+	{
 		putchar(ch);
-	ch++
-		putchar('\n');
+		ch++;
+	}
+	putchar('\n');
 	return (0);
 }
