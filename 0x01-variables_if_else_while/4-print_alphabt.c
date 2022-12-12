11e444c732dcc671761e18bc5c8ba6c12c056989
xray070 <emmanuelansah@gmail.com>
2022-12-12 04:58:41 -0800

    T4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	int ch = 'a';
+	while (ch <= 'z');
+	if ((ch != 'q') && (ch != 'e'))
+		putchar(ch);
+	ch++
+		putchar('\n');
+	return (0);
+}
