14288f6e315dc320d1fcb3e8b5e2ca2104c86979
xray070 <emmanuelansah@gmail.com>
2022-12-12 04:32:16 -0800

    T3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - low and upper alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';	
+	while (ch <= 'z')
+	{
+		putchar(ch)
+		ch++;
+	}
+	while (CH <= 'A')
+	{
+		putchar(CH)
+		CH++;
+	}
+	putchar('\n');
+	return (0)
+}
