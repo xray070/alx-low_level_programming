7fb4a76cd165cbd6a25a91fea302cd2d2e4862d5
xray070 <emmanuelansah@gmail.com>
2022-12-13 13:20:26 -0800

    T9

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,9 @@ int main(void)
 	int digit1, digit2;
 
 	for (digit1 = 0; digit1 < 9; digit1++)
-	{
-	for (digit2 = 0; digit1 < 9; digit2 < 10; digit2++
-			{
+
+		for (digit2 = 0; digit1 < 9; digit2 < 10; digit2++)
+
 			putchar((digit2 % 10) + '0');
 			putchar((digit2 % 10) + '0');
 		if (digit1 == 8 && digit2 == 9)
@@ -19,8 +19,7 @@ int main(void)
 
 		putchar (',')
 		putchar(' ')
-		}
-		}
+
 		putchar('\n')
 		return (0);
 }
