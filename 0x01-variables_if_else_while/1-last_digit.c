2ef6e3cabce969fd9e7b63d47c1e9e0ef47bdfb8
xray070 <emmanuelansah@gmail.com>
2022-12-14 05:34:00 -0800

    T1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 			n = rand() - RAND_MAX / 2;
 			/* your code goes there */
 			if (n > 0)
-				print("%d is postitive\n", n);
+				printf("%d is postitive\n", n);
 			else if (n == 0)
 				printf("%d is zero\n", n);
 			else
