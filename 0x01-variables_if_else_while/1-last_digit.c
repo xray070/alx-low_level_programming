c8263809edaef85ccce7691e7d570e7f9b20f38b
xray070 <emmanuelansah@gmail.com>
2022-12-15 07:12:47 -0800

    T1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,21 @@
-#include <stdio.h>
 #include <stdlib.h>
-
 #include <time.h>
+#include <stdio.h>
 
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
- * Return: 0
+ * main - Print if number is positive, zero or negative
+ * Return: Always (Success)
  */
 
 int main(void)
 
 {
-
 	int n;
 
 			srand(time(0));
 
 			n = rand() - RAND_MAX / 2;
-			/* your code goes there */
+
 			if (n > 0)
 			{
 				printf("%d is postitive\n", n);
@@ -33,7 +28,6 @@ int main(void)
 			{
 				printf("%d is negative\n", n);
 			}
-
-					return (0);
+			return (0);
 
 }
