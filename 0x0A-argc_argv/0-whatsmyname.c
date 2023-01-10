668c2dd173b9e7ce74ef65c05d8851cfefafd44c
xray070 <emmanuelansah9500@gmail.com>
2023-01-09 18:23:01 -0800

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Main function with args
+ * @argc: Number of args passed
+ * @argv: Array containing all command lines
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+argc, arg0v
